26977bf401fae2139a4516f218852e2668d5bf17
Michael <machado.m3@icloud.com>
2023-08-28 08:26:31 -0300

    add the service layer

--- src/main/java/com/machado/HomeCashFlow/controllers/ExpenseController.java
+++ src/main/java/com/machado/HomeCashFlow/controllers/ExpenseController.java
@@ -2,65 +2,45 @@ package com.machado.HomeCashFlow.controllers;
 
 import com.machado.HomeCashFlow.dtos.ExpenseDTO;
 import com.machado.HomeCashFlow.entities.Expense;
-import com.machado.HomeCashFlow.entities.ExpenseCategory;
-import com.machado.HomeCashFlow.repositories.ExpenseRepository;
+import com.machado.HomeCashFlow.services.ExpenseService;
 import jakarta.validation.Valid;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 public class ExpenseController {
 
     @Autowired
-    ExpenseRepository expenseRepository;
+    ExpenseService expenseService;
 
     @PostMapping("/expense")
     public ResponseEntity<Expense> save(@RequestBody @Valid ExpenseDTO expenseDTO) {
-        Expense expenseModel = new Expense();
-        BeanUtils.copyProperties(expenseDTO, expenseModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(expenseRepository.save(expenseModel));
+        return expenseService.save(expenseDTO);
     }
 
     @GetMapping("/expense")
-    public ResponseEntity<List<Expense>> getAllExpense() {
-        return ResponseEntity.status(HttpStatus.OK).body(expenseRepository.findAll());
+    public ResponseEntity<List<Expense>> getAll() {
+        return expenseService.getAll();
     }
 
     @GetMapping("/expense/{id}")
     public ResponseEntity<Object> getOne(@PathVariable(value = "id") UUID expense_id) {
-        Optional<Expense> expense = expenseRepository.findById(expense_id);
-        return expense.<ResponseEntity<Object>>map(
-                value -> ResponseEntity.status(HttpStatus.OK).body(value)).orElseGet(
-                () -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("Expense not found."));
+        return expenseService.getOne(expense_id);
     }
 
     @PutMapping("/expense/{id}")
     public ResponseEntity<Object> update(@PathVariable(value = "id") UUID expense_id,
                                          @RequestBody @Valid ExpenseDTO expenseDTO) {
-        Optional<Expense> expense = expenseRepository.findById(expense_id);
-        if (expense.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Expense not found.");
-        }
-        BeanUtils.copyProperties(expenseDTO, expense.get());
-        return ResponseEntity.status(HttpStatus.OK).body(expenseRepository.save(expense.get()));
+        return expenseService.update(expense_id, expenseDTO);
     }
 
     @DeleteMapping("/expense/{id}")
-    public ResponseEntity<Object> delete(@PathVariable(value = "id")UUID expense_id){
-        Optional<Expense> expense = expenseRepository.findById(expense_id);
-        if(expense.isEmpty()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Expense not found.");
-        }
-        expenseRepository.delete(expense.get());
-        return ResponseEntity.status(HttpStatus.OK).body("Expense deleted successfully.");
+    public ResponseEntity<Object> delete(@PathVariable(value = "id") UUID expense_id) {
+      return expenseService.delete(expense_id);
     }
 
 
---
+++ src/main/java/com/machado/HomeCashFlow/services/ExpenseService.java
@@ -0,0 +1,55 @@
+package com.machado.HomeCashFlow.services;
+
+import com.machado.HomeCashFlow.dtos.ExpenseDTO;
+import com.machado.HomeCashFlow.entities.Expense;
+import com.machado.HomeCashFlow.repositories.ExpenseRepository;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class ExpenseService {
+    @Autowired
+    ExpenseRepository expenseRepository;
+
+    public ResponseEntity<Expense> save(ExpenseDTO expenseDTO) {
+        Expense expenseModel = new Expense();
+        BeanUtils.copyProperties(expenseDTO, expenseModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(expenseRepository.save(expenseModel));
+    }
+
+    public ResponseEntity<List<Expense>> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(expenseRepository.findAll());
+    }
+
+    public ResponseEntity<Object> getOne(UUID expense_id) {
+        Optional<Expense> expense = expenseRepository.findById(expense_id);
+        return expense.<ResponseEntity<Object>>map(
+                value -> ResponseEntity.status(HttpStatus.OK).body(value)).orElseGet(
+                () -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("Expense not found."));
+    }
+
+    public ResponseEntity<Object> update(UUID expense_id,
+                                         ExpenseDTO expenseDTO) {
+        Optional<Expense> expense = expenseRepository.findById(expense_id);
+        if (expense.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Expense not found.");
+        }
+        BeanUtils.copyProperties(expenseDTO, expense.get());
+        return ResponseEntity.status(HttpStatus.OK).body(expenseRepository.save(expense.get()));
+    }
+
+    public ResponseEntity<Object> delete(UUID expense_id) {
+        Optional<Expense> expense = expenseRepository.findById(expense_id);
+        if (expense.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Expense not found.");
+        }
+        expenseRepository.delete(expense.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Expense deleted successfully.");
+    }
+}
